158890cef4977b1eca8fe9f885d3d01b91f32d95
Sara Aleksieska <sa9808@student.uni-lj.si>
2022-05-26 16:53:42 +0200

    registerUser updated

--- app/src/main/java/si/uni_lj/fe/tnuv/studybuddy/MainActivity.java
+++ app/src/main/java/si/uni_lj/fe/tnuv/studybuddy/MainActivity.java
@@ -2,13 +2,30 @@ package si.uni_lj.fe.tnuv.studybuddy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private TextView register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //registerButton, change it to the name of the "register" in sign in
+        register = (TextView) findViewById(R.id.registerButton);
+        register.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.registerButton:
+                startActivity(new Intent(this, RegisterUser.class));
+                break;
+        }
     }
 }
--- app/src/main/java/si/uni_lj/fe/tnuv/studybuddy/RegisterUser.java
+++ app/src/main/java/si/uni_lj/fe/tnuv/studybuddy/RegisterUser.java
@@ -2,13 +2,67 @@ package si.uni_lj.fe.tnuv.studybuddy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
-public class RegisterUser extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegisterUser extends AppCompatActivity implements View.OnClickListener{
+
+    private TextView banner, registerButton;
+    private EditText editTextName, editTextSurname, editTextProfession,  editTextEmail,  editTextPassword, editTextConfirmPassword;
+    private ProgressBar progressBar;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_user);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        banner = (TextView) findViewById(R.id.banner);
+        banner.setOnClickListener(this);
+
+        registerButton = (Button) findViewById(R.id.registerButton);
+        registerButton.setOnClickListener(this);
+
+        editTextName = (EditText) findViewById(R.id.name);
+        editTextSurname = (EditText) findViewById(R.id.surname);
+        editTextProfession = (EditText) findViewById(R.id.profession);
+        editTextEmail = (EditText) findViewById(R.id.email);
+        editTextPassword = (EditText) findViewById(R.id.password);
+        editTextConfirmPassword = (EditText) findViewById(R.id.confirmPassword);
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.banner:
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+            case R.id.registerButton:
+                registerButton();
+                break;
+        }
+
+    }
+
+    private void registerButton() {
+        String name = editTextName.getText().toString().trim();
+        String surname = editTextSurname.getText().toString().trim();
+        String profession = editTextProfession.getText().toString().trim();
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+        String confirmPassword = editTextConfirmPassword.getText().toString().trim();
+
     }
 }
