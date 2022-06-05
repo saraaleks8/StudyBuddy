141e4a24ef44ecd20dfabed62a7dd78fbe73fb82
Sara Aleksieska <sa9808@student.uni-lj.si>
2022-06-05 16:58:29 +0200

    child fragments added to focus page

--- app/src/main/java/si/uni_lj/fe/tnuv/studybuddy/FocusFragment.java
+++ app/src/main/java/si/uni_lj/fe/tnuv/studybuddy/FocusFragment.java
@@ -8,12 +8,36 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class FocusFragment extends Fragment {
+
+    TabLayout tabLayout;
+    ViewPager viewPager;
+
+    NewSessionChildFragment newSession = new NewSessionChildFragment();
+    BreakPlanningChildFragment breakPlan = new BreakPlanningChildFragment();
+    FocusPlanChildFragment focusPlan = new FocusPlanChildFragment();
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_focus, container, false);
+        View view = inflater.inflate(R.layout.fragment_focus, container, false);
+        addFragment(view);
+        return view;
+    }
+
+    private void addFragment(View view) {
+//        tabLayout = view.findViewById(R.id.tablayout);
+        viewPager = view.findViewById(R.id.viewPager);
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getChildFragmentManager());
+        adapter.addFragment(newSession, "New Session");
+        adapter.addFragment(breakPlan, "Break Plan");
+        adapter.addFragment(focusPlan, "Focus Plan");
+        viewPager.setAdapter(adapter);
     }
 
     @Override
@@ -22,7 +46,13 @@ public class FocusFragment extends Fragment {
 //        changeFragment();
     }
 
+    private void insertNestedFragment() {
+        Fragment childFragment = new NewSessionChildFragment();
+        FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
+        transaction.replace(R.id.childFragmentContainerFocus, childFragment).commit();
+    }
+
     private void changeFragment(Fragment fragment){
-        getChildFragmentManager().beginTransaction().replace(R.id.fragmentContainer, fragment).commitAllowingStateLoss();
+        getChildFragmentManager().beginTransaction().replace(R.id.childFragmentContainerFocus, fragment).commitAllowingStateLoss();
     }
 }
---
+++ app/src/main/java/si/uni_lj/fe/tnuv/studybuddy/NewSessionChildFragment.java
@@ -0,0 +1,17 @@
+package si.uni_lj.fe.tnuv.studybuddy;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class NewSessionChildFragment extends Fragment {
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.child_fragment_new_session, container, false);
+    }
+}
